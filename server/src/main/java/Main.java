bf05fb8e1a9c11b95060791cb3d26ab75bc179e5
Amisha <amishac55@gmail.com>
2024-12-01 18:29:27 -0700

    MAIINNNN

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,9 @@
-import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("♕ 240 Chess Server");
+        Server server = new Server();
+        int port = server.run(8080);
     }
 }
